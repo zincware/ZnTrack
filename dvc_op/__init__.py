77ae58759e5a2a3c8c2633c076a470dc91b3dbe8
Fabian <46721498+PythonFZ@users.noreply.github.com>
2021-06-02 19:43:43 +0200

    add SlurmConfig

--- dvc_op/__init__.py
+++ dvc_op/__init__.py
@@ -1,3 +1,3 @@
 """ Standard python init file for the main directory """
 from .core.dvc_op import DVCOp
-from .core.dataclasses import DVCParams
+from .core.dataclasses import DVCParams, SlurmConfig
