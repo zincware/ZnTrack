9387d8c4c07cbda484888471424cc8fe666d1050
PythonFZ <fabian.zills@web.de>
2021-06-07 17:19:00 +0200

    solve refactoring issue

--- CI/integration_tests/test_class.py
+++ CI/integration_tests/test_class.py
@@ -57,7 +57,7 @@ class TestBasic(TestCase):
     def tearDownClass(cls) -> None:
         """Remove all test files"""
         subprocess.check_call(['dvc', 'destroy', "-f"])
-        os.chdir('../../tests')
+        os.chdir('..')
         shutil.rmtree(tmp_dir)
 
     def test_query_by_id(self):
