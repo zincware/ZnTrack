8548bb02e21f6fc70af1a08c6f142d61eedb9650
SamTov <tovey.samuel@gmail.com>
2021-06-07 17:13:56 +0200

    Add docs and CI. Update .gitignore

---
+++ CI/unit_tests/core/test_py_track.py
@@ -0,0 +1,13 @@
+"""
+Test the py_track module.
+"""
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
