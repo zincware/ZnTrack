0cccf2bf793f81abb90b27b553ebaefe2eddc27f
PythonFZ <fabian.zills@web.de>
2021-11-23 16:05:05 +0100

    prepare for review

--- zntrack/utils/config.py
+++ zntrack/utils/config.py
@@ -14,7 +14,17 @@ from pathlib import Path
 
 @dataclass
 class Config:
-    """Collection of Node configurations"""
+    """Collection of Node configurations
+
+    Attributes
+    ----------
+    debug: bool
+        TODO
+    nb_name: str
+        Name of the JupyterNotebook, if the Nodes are defined in a Notebook
+    nb_class_path: Path
+        The path where jupyter notebooks should write the *.py
+    """
 
     debug: bool = False  # not implemented yet
     nb_name: str = None
