117f442bb93fba655c70d83db668f7fcd4a20777
Fabian <46721498+PythonFZ@users.noreply.github.com>
2021-12-09 14:37:09 +0100

    add test for timeit

---
+++ CI/unit_tests/metadata/test_timeit.py
@@ -0,0 +1,38 @@
+"""
+This program and the accompanying materials are made available under the terms of the
+Eclipse Public License v2.0 which accompanies this distribution, and is available at
+https://www.eclipse.org/legal/epl-v20.html
+SPDX-License-Identifier: EPL-2.0
+
+Copyright Contributors to the Zincware Project.
+
+Description: 
+"""
+from unittest.mock import Mock
+from zntrack.metadata.decorators import TimeIt
+
+
+def cls_method(self):
+    """Any cls method that will be timed"""
+    pass
+
+
+def test_timeit():
+    """Assume that metadata is empty and this is the first run"""
+    time_it = TimeIt(cls_method)
+    cls_mock = Mock()
+    cls_mock.metadata = {}
+    time_it(cls_mock)
+
+    assert cls_mock.metadata == {"cls_method:timeit": 0.0}
+
+
+def test_timeit_multi_run():
+    """Run timeit multiple times and append the metadata"""
+    time_it = TimeIt(cls_method)
+    cls_mock = Mock()
+    cls_mock.metadata = {}
+    time_it(cls_mock)
+    time_it(cls_mock)
+
+    assert cls_mock.metadata == {"cls_method:timeit": 0.0, "cls_method_1:timeit": 0.0}
