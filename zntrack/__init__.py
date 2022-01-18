5008f16e4ebea743acdedc5d0c5b1b2d01df4635
Fabian <46721498+PythonFZ@users.noreply.github.com>
2022-01-18 14:38:45 +0100

    bump version

--- zntrack/__init__.py
+++ zntrack/__init__.py
@@ -34,7 +34,7 @@ except AttributeError:
 #
 __all__ = ["Node", "ZnTrackProject", "DVCInterface", "config"]
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
