e5ec7b90418c384a4377601a44776112f1fed73b
Fabian <46721498+PythonFZ@users.noreply.github.com>
2022-01-26 09:26:48 +0100

    add tests and fix posix issue on windows

--- tests/unit_tests/core/test_dvcgraph.py
+++ tests/unit_tests/core/test_dvcgraph.py
@@ -1,7 +1,14 @@
 import pathlib
 
+import pytest
+
 from zntrack import dvc, zn
-from zntrack.core.dvcgraph import GraphWriter, get_dvc_arguments, handle_deps
+from zntrack.core.dvcgraph import (
+    GraphWriter,
+    get_dvc_arguments,
+    handle_deps,
+    handle_dvc,
+)
 
 
 class ExampleDVCOutsNode(GraphWriter):
@@ -23,6 +30,32 @@ def test_get_dvc_arguments():
 
 def test_handle_deps():
     assert handle_deps(["a.dat", "b.dat"]) == ["--deps", "a.dat", "--deps", "b.dat"]
+
+
+def test_handle_deps_posix():
+    """Change the path to posix for e.g. Windows users"""
+    assert handle_deps(r"src\file.txt") == ["--deps", "src/file.txt"]
+
+
+def test_handle_dvc():
+    assert handle_dvc(r"src\file.txt", "outs") == ["--outs", "src/file.txt"]
+    assert handle_dvc(["src/file.txt", "outs.txt"], "outs") == [
+        "--outs",
+        "src/file.txt",
+        "--outs",
+        "outs.txt",
+    ]
+    assert handle_dvc(("src/file.txt", "outs.txt"), "outs") == [
+        "--outs",
+        "src/file.txt",
+        "--outs",
+        "outs.txt",
+    ]
+
+
+def test_handle_deps_unknown():
+    with pytest.raises(ValueError):
+        handle_deps(handle_deps)
 
 
 def test_handle_deps_node():
