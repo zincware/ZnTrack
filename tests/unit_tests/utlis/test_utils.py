177594931c4a91c9ba2377948e0b46e1576197a1
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2022-02-03 21:02:59 +0100

    Add automatic __init__ (#198)
    * fix merge conflicts
    
    * Added __signature__ to sub-class
    
    Previously the signature shown in i.e. Jupyter showed only **kwargs as  arguments, now all zn.params arre listed by adding a __signature__ to the delegate.
    
    * fix lint
    
    Co-authored-by: JR-1991 <30547301+JR-1991@users.noreply.github.com>

--- tests/unit_tests/utlis/test_utils.py
+++ tests/unit_tests/utlis/test_utils.py
@@ -14,13 +14,14 @@ import pathlib
 import sys
 from unittest.mock import MagicMock, patch
 
+import pytest
 import znjson
 
-from zntrack.utils.utils import cwd_temp_dir, decode_dict, module_handler
+from zntrack.utils import utils
 
 
 def test_cwd_temp_dir():
-    new_dir = cwd_temp_dir(required_files=[__file__])
+    new_dir = utils.cwd_temp_dir(required_files=[__file__])
     assert pathlib.Path(new_dir.name) == pathlib.Path(os.getcwd())
     assert next(pathlib.Path(new_dir.name).glob("*.py")).name == "test_utils.py"
     os.chdir("..")
@@ -32,18 +33,34 @@ def test_decode_dict_path():
     dict_string = json.dumps(path, cls=znjson.ZnEncoder)
     loaded_dict = json.loads(dict_string)
     assert loaded_dict == {"_type": "pathlib.Path", "value": "test.txt"}
-    assert decode_dict(loaded_dict) == path
-    assert decode_dict(None) is None
+    assert utils.decode_dict(loaded_dict) == path
+    assert utils.decode_dict(None) is None
+
+
+class Test:
+    pass
+
+
+def test_get_auto_init():
+    with pytest.raises(TypeError):
+        Test(foo="foo")
+
+    setattr(Test, "__init__", utils.get_auto_init(fields=["foo", "bar"]))
+
+    test = Test(foo="foo", bar="bar")
+
+    assert test.foo == "foo"
+    assert test.bar == "bar"
 
 
 def test_module_handler():
     my_mock = MagicMock
     my_mock.__module__ = "custom_module"
-    assert module_handler(my_mock) == "custom_module"
+    assert utils.module_handler(my_mock) == "custom_module"
 
     my_mock.__module__ = "__main__"
     with patch.object(sys, "argv", ["ipykernel_launcher"]):
-        assert module_handler(my_mock) == "__main__"
+        assert utils.module_handler(my_mock) == "__main__"
 
     with patch.object(sys, "argv", ["pytest-runner"]):
-        assert module_handler(my_mock) == "pytest-runner"
+        assert utils.module_handler(my_mock) == "pytest-runner"
