0792a2bda55a402315a9d36bfbe49b9af7db7cea
PythonFZ <fabian.zills@web.de>
2022-03-02 15:43:50 +0100

    add NodeConfig dotdict test
    - test NodeConfig.convert_fields_to_dotdict()

--- tests/unit_tests/core/test_decorator.py
+++ tests/unit_tests/core/test_decorator.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pytest
 
 from zntrack.core.functions.decorator import NodeConfig
@@ -23,3 +25,24 @@ def test_not_supported_params():
 
 def test_supported_params():
     assert NodeConfig(params={"name": "John"}).params["name"] == "John"
+
+
+def test_NodeConfig_convert_fields_to_dotdict():
+    cfg = NodeConfig(outs="file")
+    assert cfg.outs == "file"
+
+    cfg = NodeConfig(outs={"file": "file.txt"})
+    cfg.convert_fields_to_dotdict()
+    assert cfg.outs.file == "file.txt"
+    assert cfg.outs["file"] == "file.txt"
+
+    cfg = NodeConfig(outs={"files": {"data": "datafile.txt"}})
+    cfg.convert_fields_to_dotdict()
+    assert cfg.outs.files.data == "datafile.txt"
+
+    cfg = NodeConfig(
+        outs={"files": {"file": "datafile.txt", "path": pathlib.Path("data.txt")}}
+    )
+    cfg.convert_fields_to_dotdict()
+    assert cfg.outs.files.file == "datafile.txt"
+    assert cfg.outs.files.path == pathlib.Path("data.txt")
