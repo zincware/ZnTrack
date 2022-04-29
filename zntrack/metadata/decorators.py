aed428c8c67db658b234c51bba73bc48fd36e261
PythonFZ <fabian.zills@web.de>
2022-04-29 14:05:04 +0200

    add mean/std to timeit decorator

--- zntrack/metadata/decorators.py
+++ zntrack/metadata/decorators.py
@@ -1,6 +1,22 @@
+import dataclasses
+import statistics
 from time import time
 
-from .base import MetaData
+from zntrack.metadata.base import MetaData
+
+
+@dataclasses.dataclass
+class AggregateData:
+    values: list = dataclasses.field(default_factory=list)
+    mean: float = None
+    std: float = None
+
+    def update(self):
+        try:
+            self.mean = statistics.mean(self.values)
+            self.std = statistics.stdev(self.values)
+        except statistics.StatisticsError:
+            pass
 
 
 class TimeIt(MetaData):
@@ -16,6 +32,16 @@ class TimeIt(MetaData):
         parsed_func = self.func(cls, *args, **kwargs)
         stop_time = time()
 
-        self.save_metadata(cls, value=stop_time - start_time)
+        value = stop_time - start_time
+
+        metadata, _ = self.get_history(cls)
+        history = metadata.get(self.name, AggregateData())
+        if isinstance(history, dict):
+            history = AggregateData(**history)
+        history.values.append(value)
+        history.update()
+        history = dataclasses.asdict(history)
+
+        self.save_metadata(cls, value=history)
 
         return parsed_func
