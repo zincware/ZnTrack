66a4830d7369178b9956e279c680e7072d7de2e9
PythonFZ <fabian.zills@web.de>
2022-05-20 12:56:04 +0200

    add `zntrack init --force`

--- tests/integration_tests/test_cli.py
+++ tests/integration_tests/test_cli.py
@@ -21,6 +21,8 @@ def test_init(proj_path):
     assert (pathlib.Path("src") / "__init__.py").is_file()
     assert pathlib.Path(".git").is_dir()
     assert pathlib.Path(".dvc").is_dir()
+    assert pathlib.Path("main.py").is_file()
+    assert pathlib.Path("README.md").is_file()
 
 
 def test_init_gitignore(proj_path):
@@ -31,3 +33,26 @@ def test_init_gitignore(proj_path):
     assert pathlib.Path(".git").is_dir()
     assert pathlib.Path(".dvc").is_dir()
     assert pathlib.Path(".gitignore").is_file()
+    assert pathlib.Path("main.py").is_file()
+    assert pathlib.Path("README.md").is_file()
+
+
+@pytest.mark.parametrize("force", (True, False))
+def test_init_force(proj_path, force):
+    pathlib.Path("file.txt").touch()
+
+    if force:
+        subprocess.check_call(["zntrack", "init", "--force"])
+        assert pathlib.Path("src").is_dir()
+        assert (pathlib.Path("src") / "__init__.py").is_file()
+        assert pathlib.Path(".git").is_dir()
+        assert pathlib.Path(".dvc").is_dir()
+        assert pathlib.Path("main.py").is_file()
+        assert pathlib.Path("README.md").is_file()
+    else:
+        subprocess.check_call(["zntrack", "init"])
+        assert not pathlib.Path("src").exists()
+        assert not pathlib.Path(".git").exists()
+        assert not pathlib.Path(".dvc").exists()
+        assert not pathlib.Path("main.py").exists()
+        assert not pathlib.Path("README.md").exists()
--- zntrack/cli/init.py
+++ zntrack/cli/init.py
@@ -14,6 +14,7 @@ app = typer.Typer()
 class Initializer:
     name: str
     gitignore: bool
+    force: bool
     src: pathlib.Path = pathlib.Path("src")
 
     _gitignore_url: str = (
@@ -21,11 +22,29 @@ class Initializer:
     )
 
     def run(self):
+        self.check_empty()
         typer.echo(f"Creating new project: {self.name}")
         self.make_src()
         self.make_repo()
+        self.write_main_file()
+        self.write_readme()
         if self.gitignore:
             self.write_gitignore()
+
+    def check_empty(self):
+        """Check if the project directory is empty
+
+        Raises
+        ------
+        typer.Exit: if the directory is not empty and force is false
+        """
+        is_empty = not any(pathlib.Path(".").iterdir())
+        if not is_empty and not self.force:
+            typer.echo(
+                "The current working directory is not empty. If you want to initialize a"
+                " project anyway use 'zntrack init --force'."
+            )
+            raise typer.Exit()
 
     def make_src(self):
         self.src.mkdir()
@@ -36,6 +55,12 @@ class Initializer:
         with urllib.request.urlopen(self._gitignore_url) as url:
             gitignore.write_text(url.read().decode("utf-8"))
 
+    def write_main_file(self):
+        pathlib.Path("main.py").touch()
+
+    def write_readme(self):
+        pathlib.Path("README.md").write_text(f"# Welcome to {self.name}")
+
     def make_repo(self):
         subprocess.check_call(["git", "init"])
         subprocess.check_call(["dvc", "init"])
@@ -43,15 +68,18 @@ class Initializer:
 
 @app.command()
 def init(
-    name: str = "MyProject",
+    name: str = "New Project",
     gitignore: bool = typer.Option(
         False, help="Create a gitignore file with all the usual files excluded."
+    ),
+    force: bool = typer.Option(
+        False, "--force", "-f", help="Initialize Project if the directory is not empty."
     ),
 ):
     """
     <update me>
     """
-    initializer = Initializer(name=name, gitignore=gitignore)
+    initializer = Initializer(name=name, gitignore=gitignore, force=force)
     initializer.run()
 
 
