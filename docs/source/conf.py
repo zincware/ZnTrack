be10b33997bfaa30eeba14d29848e1a35a485e0c
PythonFZ <fabian.zills@web.de>
2022-05-30 13:18:45 +0200

    poetry update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,10 +81,7 @@ html_theme_options = {
 }
 html_logo = "img/zntrack.png"
 html_favicon = "img/zntrack_favicon.png"
-html_theme_options = {
-    "logo_only": True,
-    "display_version": False,
-}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
