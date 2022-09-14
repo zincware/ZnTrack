233be5a4bd3f0d1eae4e46db00d8128e85e13363
PythonFZ <fabian.zills@web.de>
2022-09-14 15:58:29 +0200

    add load test

--- tests/integration_tests/test_loading_exceptions.py
+++ tests/integration_tests/test_loading_exceptions.py
@@ -32,11 +32,13 @@ def test_GraphNotAvailableError(model):
         _ = node_obj.output
 
 
-def test_DataNotAvailableError(proj_path):
+@pytest.mark.parametrize("load", (True, False))
+def test_DataNotAvailableError(proj_path, load):
     node_obj = WriteNumber(input=5)
     node_obj.write_graph()
 
-    node_obj = WriteNumber.load()
+    if load:
+        node_obj = WriteNumber.load()
     assert node_obj.input == 5
     with pytest.raises(zntrack.exceptions.DataNotAvailableError):
         _ = node_obj.output
