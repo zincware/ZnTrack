9399bae8965c2acac3078f161c4b2765462484c2
PythonFZ <fabian.zills@web.de>
2022-10-06 15:19:31 +0200

    reverse change in load vs getitem

--- zntrack/core/base.py
+++ zntrack/core/base.py
@@ -564,7 +564,7 @@ class Node(NodeBase, metaclass=LoadViaGetItem):
             custom_args += pair
 
         if call_args is None:
-            call_args = f"['{self.node_name}'].run_and_save()"
+            call_args = f".load(name='{self.node_name}').run_and_save()"
 
         script = prepare_dvc_script(
             node_name=self.node_name,
