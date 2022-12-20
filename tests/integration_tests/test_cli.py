1e0d85c9c697a3d3574dae3b4d5ef5566bc206a9
PythonFZ <fabian.zills@web.de>
2022-12-20 16:51:22 +0100

    add cli test

--- tests/integration_tests/test_cli.py
+++ tests/integration_tests/test_cli.py
@@ -4,6 +4,7 @@ import pathlib
 import pytest
 from typer.testing import CliRunner
 
+from zntrack import Node, NodeConfig, nodify, zn
 from zntrack.cli import app
 
 
@@ -63,3 +64,45 @@ def test_init_force(tmp_path, runner, force):
         assert not pathlib.Path(".dvc").exists()
         assert not pathlib.Path("main.py").exists()
         assert not pathlib.Path("README.md").exists()
+
+
+class InputsToOutputs(Node):
+    inputs = zn.params()
+    outputs = zn.outs()
+
+    def run(self):
+        self.outputs = self.inputs
+
+
+@nodify(outs="test.txt", params={"text": "Lorem Ipsum"})
+def example_func(cfg: NodeConfig) -> NodeConfig:
+    out_file = pathlib.Path(cfg.outs)
+    out_file.write_text(cfg.params.text)
+    return cfg
+
+
+def test_run(proj_path, runner):
+    node = InputsToOutputs(inputs=15)
+    node.write_graph()
+    result = runner.invoke(app, ["run", "test_cli.InputsToOutputs"])
+    assert result.exit_code == 0
+
+    node = InputsToOutputs.load()
+    assert node.outputs == 15
+
+
+def test_run_nodify(proj_path, runner):
+    example_func()
+    result = runner.invoke(app, ["run", "test_cli.example_func"])
+    assert result.exit_code == 0
+    assert pathlib.Path("test.txt").read_text() == "Lorem Ipsum"
+
+
+def test_run_w_name(proj_path, runner):
+    node = InputsToOutputs(inputs=15, name="TestNode")
+    node.write_graph()
+    result = runner.invoke(app, ["run", "test_cli.InputsToOutputs", "--name", "TestNode"])
+    assert result.exit_code == 0
+
+    node = InputsToOutputs["TestNode"]
+    assert node.outputs == 15
