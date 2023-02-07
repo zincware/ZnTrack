8fcdd0957d2f15e3f195eb5bd9943bba192a1698
PythonFZ <fabian.zills@web.de>
2023-02-07 13:41:57 +0100

    add nodify

--- zntrack/core/__init__.py
+++ zntrack/core/__init__.py
@@ -1,5 +1,7 @@
 """Core module of zntrack."""
 from zntrack.core.base import Node
 from zntrack.core.zntrackoption import ZnTrackOption
+from zntrack.core.functions.decorator import nodify, NodeConfig
 
-__all__ = ["Node", "ZnTrackOption"]
+__all__ = ["Node", "ZnTrackOption", "nodify", "NodeConfig"]
+
