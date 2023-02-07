b43e9317cf4825504829c756ad0feb0eb1946e65
PythonFZ <fabian.zills@web.de>
2023-02-07 14:02:40 +0100

    fix coding style

--- zntrack/core/__init__.py
+++ zntrack/core/__init__.py
@@ -1,7 +1,6 @@
 """Core module of zntrack."""
 from zntrack.core.base import Node
+from zntrack.core.functions.decorator import NodeConfig, nodify
 from zntrack.core.zntrackoption import ZnTrackOption
-from zntrack.core.functions.decorator import nodify, NodeConfig
 
 __all__ = ["Node", "ZnTrackOption", "nodify", "NodeConfig"]
-
