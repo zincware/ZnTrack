cacc5b3506c1b03feaf918116e5428fb9fc867e4
PythonFZ <fabian.zills@web.de>
2023-03-08 18:04:53 +0100

    Update Custom Field

--- zntrack/__init__.py
+++ zntrack/__init__.py
@@ -5,11 +5,11 @@ GitHub: https://github.com/zincware/ZnTrack
 import importlib.metadata
 
 from zntrack.core.node import Node
-from zntrack.fields import dvc, meta, zn
+from zntrack.fields import dvc, meta, zn, Field, LazyField
 from zntrack.project import Project
 from zntrack.utils import config
 from zntrack.utils.node_wd import nwd
 
 __version__ = importlib.metadata.version("zntrack")
 
-__all__ = ["Node", "dvc", "zn", "Project", "nwd", "meta", "config"]
+__all__ = ["Node", "dvc", "zn", "Project", "nwd", "meta", "config", "Field", "LazyField"]
--- zntrack/fields/__init__.py
+++ zntrack/fields/__init__.py
@@ -1,6 +1,6 @@
 """ZnTrack Fields."""
 
 from zntrack.fields import dvc, meta, zn
-from zntrack.fields.field import Field
+from zntrack.fields.field import Field, LazyField
 
-__all__ = ["Field", "zn", "dvc", "meta"]
+__all__ = ["Field", "zn", "dvc", "meta", "LazyField"]
