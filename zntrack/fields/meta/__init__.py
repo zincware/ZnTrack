8bd522ac5746da51d024d4014fdabb838d5a8050
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2023-03-14 21:13:18 +0100

    Add env (#521)
    * add meta.Environment
    
    * add test for `meta.Environment`
    
    * lint
    
    * add is_parameter option

--- zntrack/fields/meta/__init__.py
+++ zntrack/fields/meta/__init__.py
@@ -37,3 +37,46 @@ class Text(Field):
     def get_stage_add_argument(self, instance) -> typing.List[tuple]:
         """Get the dvc command for this field."""
         return []
+
+
+class Environment(Field):
+    """Environment variables to export."""
+
+    dvc_option: str = None
+    group = FieldGroup.PARAMETER
+
+    def __init__(self, *args, is_parameter: bool = False, **kwargs):
+        """Initialize the field."""
+        self.is_parameter = is_parameter
+        super().__init__(*args, **kwargs)
+
+    def get_affected_files(self, instance) -> list:
+        """There are no affect files."""
+        return []
+
+    def save(self, instance):
+        """Save the field to disk."""
+        file = pathlib.Path("env.yaml")
+        try:
+            context = yaml.safe_load(file.read_text())
+        except FileNotFoundError:
+            context = {}
+
+        node_context = context.get(instance.name, {})
+        value = getattr(instance, self.name)
+        if not isinstance(value, str):
+            raise ValueError(f"Environment value must be a string, not {type(value)}")
+        node_context[self.name] = value
+        context[instance.name] = node_context
+        file.write_text(yaml.safe_dump(context))
+
+    def get_data(self, instance: "Node") -> any:
+        """Get the value of the field from the file."""
+        env_dict = yaml.safe_load(instance.state.get_file_system().read_text("env.yaml"))
+        return env_dict.get(instance.name, {}).get(self.name, None)
+
+    def get_stage_add_argument(self, instance) -> typing.List[tuple]:
+        """Get the dvc command for this field."""
+        if self.is_parameter:
+            return [("--params", f"env.yaml:{instance.name}.{self.name}")]
+        return []
