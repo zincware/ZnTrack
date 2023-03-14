140d436b8162978efcd1e87a4028a8551efe64d4
PythonFZ <fabian.zills@web.de>
2023-03-14 21:22:47 +0100

    add tests

--- tests/integration/test_project.py
+++ tests/integration/test_project.py
@@ -71,3 +71,56 @@ def test_WriteIO_no_name(tmp_path_2, assert_before_exp):
 
     assert exp2["WriteIO"].inputs == "Lorem Ipsum"
     assert exp2["WriteIO"].outputs == "Lorem Ipsum"
+
+
+class CombineOutputs(zntrack.Node):
+    inputs = zntrack.zn.deps()
+    outputs = zntrack.zn.outs()
+
+    def run(self) -> None:
+        self.outputs = "".join([x.outputs for x in self.inputs])
+
+
+def test_load_between_graphs(proj_path):
+    """Load a Node with dependencies between graph runs."""
+    with zntrack.Project() as project:
+        node1 = WriteIO(inputs="A", name="node1")
+        node2 = WriteIO(inputs="B", name="node2")
+        node3 = CombineOutputs(inputs=[node1, node2], name="node3")
+
+    project.run()
+
+    node3.load()
+    assert node3.outputs == "AB"
+
+    with project:
+        node4 = CombineOutputs(inputs=[node1, node3], name="node4")
+
+    project.run()
+
+    node4.load()
+    assert node4.outputs == "AAB"
+
+
+def test_multi_exp(tmp_path_2):
+    with zntrack.Project() as project:
+        node1 = WriteIO(inputs="A", name="node1")
+        node2 = WriteIO(inputs="B", name="node2")
+
+    project.run()
+
+    with project.create_experiment() as exp1:
+        node1.inputs = "A1"
+
+    with project.create_experiment() as exp2:
+        node2.inputs = "B1"
+
+    project.run_exp()
+
+    exp1.load()
+    assert exp1["node1"].outputs == "A1"
+    assert exp1["node2"].outputs == "B"
+
+    exp2.load()
+    assert exp2["node1"].outputs == "A"
+    assert exp2["node2"].outputs == "B1"
