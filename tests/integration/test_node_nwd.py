da9ee7b54796b2d34c5074bb74de5ea1159c610f
PythonFZ <fabian.zills@web.de>
2023-03-17 15:26:13 +0100

    fix #526

--- tests/integration/test_node_nwd.py
+++ tests/integration/test_node_nwd.py
@@ -12,6 +12,13 @@ class WriteToNWD(zntrack.Node):
     def run(self):
         self.nwd.mkdir(exist_ok=True, parents=True)
         self.file[0].write_text(self.text)
+    
+class OutsAsNWD(zntrack.Node):
+    text = zntrack.zn.params()
+    outs: pathlib.Path = zntrack.dvc.outs(zntrack.nwd)
+
+    def run(self):
+        (self.outs / "test.txt").write_text(self.text)
 
 
 @pytest.mark.parametrize("eager", [True, False])
@@ -25,3 +32,15 @@ def test_WriteToNWD(proj_path, eager):
     if not eager:
         write_to_nwd.load()
     assert write_to_nwd.__dict__["file"] == [pathlib.Path("$nwd$", "test.txt")]
+
+@pytest.mark.parametrize("eager", [True, False])
+def test_OutAsNWD(proj_path, eager):
+    with zntrack.Project() as project:
+        outs_as_nwd = OutsAsNWD(text="Hello World")
+
+    project.run(eager=eager)
+    assert (outs_as_nwd.outs / "test.txt").read_text() == "Hello World"
+    assert outs_as_nwd.outs == pathlib.Path("nodes", "OutsAsNWD")
+    if not eager:
+        outs_as_nwd.load()
+    assert outs_as_nwd.__dict__["outs"] == zntrack.nwd
