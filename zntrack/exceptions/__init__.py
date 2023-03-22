fb922c6afe75f5f5285a955d734c75b93bfc5c63
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2023-03-22 18:44:32 +0100

    add remove existing graph arg to zntrack.Project (#544)
    * add exception and remove_existing_graph
    
    * change check

---
+++ zntrack/exceptions/__init__.py
@@ -0,0 +1,19 @@
+"""All ZnTrack exceptions."""
+
+
+class NodeNotAvailableError(Exception):
+    """Raised when a node is not available."""
+
+    def __init__(self, arg):
+        """Initialize the exception.
+
+        Parameters
+        ----------
+        arg : str|Node
+            Custom Error message or Node that is not available.
+        """
+        if isinstance(arg, str):
+            super().__init__(arg)
+        else:
+            # assume arg is a Node
+            super().__init__(f"Node {arg.name} is not available.")
