142d3e204e520ef0016fbbefd0ccf6d1d7e48388
PythonFZ <fabian.zills@web.de>
2023-04-04 17:20:11 +0200

    add tests and exception

--- zntrack/exceptions/__init__.py
+++ zntrack/exceptions/__init__.py
@@ -17,3 +17,21 @@ class NodeNotAvailableError(Exception):
         else:
             # assume arg is a Node
             super().__init__(f"Node {arg.name} is not available.")
+
+
+class ZnNodesOnGraphError(Exception):
+    """Raised when a node is not available."""
+
+    def __init__(self, arg):
+        """Initialize the exception.
+
+        Parameters
+        ----------
+        arg : str|Node
+            Custom Error message or Node that is not available.
+        """
+        if isinstance(arg, str):
+            super().__init__(arg)
+        else:
+            # assume arg is a Node
+            super().__init__(f"Node {arg.name} is not available.")
