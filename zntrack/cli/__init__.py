19f98ca0cfd817fbb04e3d4daddf1cc586e8d182
PythonFZ <fabian.zills@web.de>
2023-04-06 20:40:11 +0200

    allow dict

--- zntrack/cli/__init__.py
+++ zntrack/cli/__init__.py
@@ -60,6 +60,12 @@ def run(node: str, name: str = None, hash_only: bool = False) -> None:
         for key, value in env.get("stages", {}).get(name, {}).items():
             if isinstance(value, str):
                 os.environ[key] = value
+            elif isinstance(value, dict):
+                os.environ.update(value)
+            elif value is None:
+                pass
+            else:
+                raise ValueError(f"Unknown value for env variable {key}: {value}")
 
     sys.path.append(pathlib.Path.cwd().as_posix())
 
