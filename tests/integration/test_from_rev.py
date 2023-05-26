625ba4260002e0035e74a92e59df24588d3baa54
PythonFZ <fabian.zills@web.de>
2023-05-26 15:19:50 +0200

    test module / wrong rev

---
+++ tests/integration/test_from_rev.py
@@ -0,0 +1,22 @@
+import dvc.scm
+import pytest
+
+import zntrack
+
+
+def test_module_not_installed():
+    with pytest.raises(ModuleNotFoundError):
+        zntrack.from_rev(
+            "ASEMD",
+            remote="https://github.com/IPSProjects/IPS-Water",
+            rev="ca0eef0ccfcbfb72a82136849a9ca35eac8b7629",
+        )
+
+
+def test_commit_not_found():
+    with pytest.raises(dvc.scm.RevError):
+        zntrack.from_rev(
+            "ASEMD",
+            remote="https://github.com/IPSProjects/IPS-Water",
+            rev="this-does-not-exist",
+        )
