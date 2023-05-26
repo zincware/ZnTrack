55765d15013dea016f34e31bbb696c61e2e6ff9f
PythonFZ <fabian.zills@web.de>
2023-05-26 16:03:34 +0200

    try loading from temporary file

--- tests/integration/test_from_rev.py
+++ tests/integration/test_from_rev.py
@@ -20,3 +20,14 @@ def test_commit_not_found():
             remote="https://github.com/IPSProjects/IPS-Water",
             rev="this-does-not-exist",
         )
+
+
+def test_import_from_remote():
+    node = zntrack.from_rev(
+        "HelloWorld",
+        remote="https://github.com/PythonFZ/ZnTrackExamples.git",
+        rev="b9316bf",
+    )
+    assert node.max_number == 512
+    assert node.random_number == 64
+    assert node.name == "HelloWorld"
--- zntrack/core/load.py
+++ zntrack/core/load.py
@@ -2,6 +2,10 @@
 
 import contextlib
 import importlib
+import importlib.util
+import pathlib
+import sys
+import tempfile
 import typing
 
 import dvc.api
@@ -25,6 +29,43 @@ def _get_stage(name, remote, rev) -> dvc.stage.PipelineStage:
         raise ValueError(
             f"Stage {name} not found in {remote}" + (f"/tree/{rev}" if rev else "")
         )
+
+
+def _import_from_tempfile(package_and_module: str, remote, rev) -> typing.ModuleType:
+    """Create a temporary file to import from.
+
+    Parameters
+    ----------
+    package_and_module : str
+        The package and module to import, e.g. "zntrack.core.node.Node".
+    remote : str
+        The remote to load the module from.
+    rev : str
+        The revision to load the module from.
+
+    Returns
+    -------
+    ModuleType
+        The imported module.
+
+    Raises
+    ------
+    ModuleNotFoundError
+        If the module could not be found.
+    FileNotFoundError
+        If the file could not be found.
+    """
+    file = pathlib.Path(*package_and_module.split(".")).with_suffix(".py")
+    fs = dvc.api.DVCFileSystem(url=remote, rev=rev)
+    with tempfile.NamedTemporaryFile(suffix=".py") as temp_file, fs.open(file) as f:
+        temp_file.write(f.read())
+        temp_file.flush()
+
+        spec = importlib.util.spec_from_file_location(package_and_module, temp_file.name)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[package_and_module] = module
+        spec.loader.exec_module(module)
+        return module
 
 
 def from_rev(name, remote=".", rev=None, **kwargs) -> T:
@@ -53,14 +94,19 @@ def from_rev(name, remote=".", rev=None, **kwargs) -> T:
     name = cmd.split()[4]
 
     package_and_module, cls_name = run_str.rsplit(".", 1)
+    module = None
     try:
         module = importlib.import_module(package_and_module)
-    except ModuleNotFoundError as err:
+    except ModuleNotFoundError:
+        module = _import_from_tempfile(package_and_module, remote, rev)
+        # with contextlib.suppress(FileNotFoundError, ModuleNotFoundError):
+
+    if module is None:
         module_name = package_and_module.split(".")[0]
         raise ModuleNotFoundError(
             f"No module named '{module_name}'. The package might be available via 'pip"
             f" install {module_name}' or from the remote via 'pip install git+{remote}'."
-        ) from err
+        )
 
     cls = getattr(module, cls_name)
 
