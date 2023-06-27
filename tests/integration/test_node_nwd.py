b3a370ab7e245e1771967de3ae40bfc1e06453db
PythonFZ <fabian.zills@web.de>
2023-06-27 16:02:19 +0200

    remove commented out code

--- tests/integration/test_node_nwd.py
+++ tests/integration/test_node_nwd.py
@@ -47,26 +47,3 @@ def test_OutAsNWD(proj_path):
         class OutsAsNWD(zntrack.Node):
             text = zntrack.zn.params()
             outs: pathlib.Path = zntrack.dvc.outs(zntrack.nwd)
-
-
-# @pytest.mark.parametrize("eager", [True, False])
-# def test_OutAsNWD(proj_path, eager):
-#     with zntrack.Project() as project:
-#         outs_as_nwd = OutsAsNWD(text="Hello World")
-
-#     project.run(eager=eager)
-#     assert (outs_as_nwd.outs / "test.txt").read_text() == "Hello World"
-#     assert outs_as_nwd.outs == pathlib.Path("nodes", "OutsAsNWD")
-#     if not eager:
-#         outs_as_nwd.load()
-#     assert outs_as_nwd.__dict__["outs"] == zntrack.nwd
-
-
-# def test_FileToOuts(proj_path):
-#     with zntrack.Project() as project:
-#         # write_to_nwd = OutsAsNWD(text="Hello World")
-#         file_to_outs = FileToOuts(file="test.txt")
-
-#     project.run()
-#     file_to_outs.load()
-#     assert file_to_outs.text == "Hello World"
