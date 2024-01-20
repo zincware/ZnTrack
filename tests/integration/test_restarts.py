7ee6c75a3557c37548178174e50c93878f9b4420
PythonFZ <fabian.zills@web.de>
2024-01-20 11:12:00 +0100

    update test

--- tests/integration/test_restarts.py
+++ tests/integration/test_restarts.py
@@ -21,18 +21,20 @@ def test_restarts(tmp_path_2):
     project = zntrack.Project()
 
     with project:
-        node = zntrack.examples.NodeWithRestart(start=0, raise_exception_until=1)
+        node = zntrack.examples.NodeWithRestart(start=0, raise_exception_until=2)
 
     project.build()
     # can not longer use project.run() because it will remove outputs, running manually
     # this should raise an exception
-    subprocess.run(
+    results = subprocess.run(
         ["zntrack", "run", "zntrack.examples.NodeWithRestart", "--name=NodeWithRestart"]
     )
+    assert results.returncode == 1
     # this should succeed
-    subprocess.run(
+    results = subprocess.run(
         ["zntrack", "run", "zntrack.examples.NodeWithRestart", "--name=NodeWithRestart"]
     )
+    assert results.returncode == 0
 
     node.load()
     assert node.count == 2
