1b9f6f9ccfab4ff69cba0f9e9afdf81cd173b9cf
Niklas Kappel <niklas.kappel@kit.edu>
2024-02-13 09:17:23 +0100

    add type hints for path fields

--- zntrack/fields/fields.pyi
+++ zntrack/fields/fields.pyi
@@ -1,10 +1,14 @@
-from typing import Any, Callable, Optional, TypeVar, overload
+import os
+from typing import Any, Callable, Optional, TypeVar, Union, overload
 
 from zninit.descriptor import Empty
 
 _T = TypeVar("_T")
+_P = TypeVar("_P", bound=Union[str, bytes, os.PathLike])
 
 def outs() -> Any: ...
+
+# TODO: metricts
 @overload
 def params(
     default: Empty = ...,
@@ -31,3 +35,40 @@ def params(
     frozen: bool = False,
     on_setattr: Optional[Callable] = ...,
 ) -> _T: ...
+
+# TODO: deps
+# TODO: plots
+@overload
+def outs_path(dvc_option: str = ..., **kwargs) -> Any: ...
+@overload
+def outs_path(args: _P, dvc_option: str = ..., **kwargs) -> _P: ...
+@overload
+def outs_path(arg1: _P, *args: _P, dvc_option: str = ..., **kwargs) -> tuple[_P, ...]: ...
+@overload
+def metrics_path(dvc_option: str = ..., **kwargs) -> Any: ...
+@overload
+def metrics_path(args: _P, dvc_option: str = ..., **kwargs) -> _P: ...
+@overload
+def metrics_path(
+    arg1: _P, *args: _P, dvc_option: str = ..., **kwargs
+) -> tuple[_P, ...]: ...
+@overload
+def params_path(**kwargs) -> Any: ...
+@overload
+def params_path(args: _P, **kwargs) -> _P: ...
+@overload
+def params_path(arg1: _P, *args: _P, **kwargs) -> tuple[_P, ...]: ...
+@overload
+def deps_path(**kwargs) -> Any: ...
+@overload
+def deps_path(args: _P, **kwargs) -> _P: ...
+@overload
+def deps_path(arg1: _P, *args: _P, **kwargs) -> tuple[_P, ...]: ...
+@overload
+def plots_path(dvc_option: str = ..., **kwargs) -> Any: ...
+@overload
+def plots_path(args: _P, dvc_option: str = ..., **kwargs) -> _P: ...
+@overload
+def plots_path(
+    arg1: _P, *args: _P, dvc_option: str = ..., **kwargs
+) -> tuple[_P, ...]: ...
