b83fb3fdfc1b1cf905cae751f5c88c393993ac69
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-08-09 00:01:45 +0200

    prepare more testing

--- tests/files/test_basic.py
+++ tests/files/test_basic.py
@@ -1,14 +1,35 @@
 import zntrack
 import pathlib
+import pandas as pd
 
 class MyNode(zntrack.Node):
     parameter: int = zntrack.params()
+    parameter_path: str = zntrack.params_path()
+
+    # deps are tested separately
+    
+    deps_path: str = zntrack.deps_path()
+    outs_path: list[str]  = zntrack.outs_path()
+    metrics_path: pathlib.Path = zntrack.metrics_path()
+    plots_path: list[pathlib.Path] = zntrack.plots_path()
+
+    outs: int = zntrack.outs()
+    metrics: dict = zntrack.metrics()
+    plots: pd.DataFrame = zntrack.plots()
+
 
 CWD = pathlib.Path(__file__).parent.resolve()
 
-def test_basic(proj_path):
+def test_basic():
     with zntrack.Project() as project:
-        _ = MyNode(parameter=1)
+        _ = MyNode(
+            parameter=1,
+            parameter_path="parameter.yaml",
+            deps_path="deps.yaml",
+            outs_path=["outs.yaml"],
+            metrics_path=zntrack.nwd / "metrics.json",
+            plots_path=[zntrack.nwd / "plots.csv"],
+        )
     
     project.build()
 
