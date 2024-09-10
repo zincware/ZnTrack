3b9c8c9752021c44b158d4f060aff7349debbdae
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-09-10 15:38:53 +0200

    sketch of add clit utils

--- zntrack/utils/__init__.py
+++ zntrack/utils/__init__.py
@@ -3,5 +3,6 @@
 from .import_handler import module_handler
 from .misc import get_attr_always_list
 from .node_wd import NWDReplaceHandler, nwd
+import zntrack.utils.cli
 
-__all__ = ["module_handler", "nwd", "get_attr_always_list", "NWDReplaceHandler"]
+__all__ = ["module_handler", "nwd", "get_attr_always_list", "NWDReplaceHandler", "cli"]
