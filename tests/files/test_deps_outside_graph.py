937c7eebd6b84349a73e5873072fc0a7e2d64c8a
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-09-11 07:05:49 +0200

    add some TODOs

--- tests/files/test_deps_outside_graph.py
+++ tests/files/test_deps_outside_graph.py
@@ -18,7 +18,15 @@ def test_deps_outside_graph(proj_path):
 
     project.build()
 
+    # TODO: the node name changes with every node the thermostat would be attached to
+    #  it might be better if it is None!
+    # TODO: assert that node.name when setting can not include a "+" character
+    # The thermostat does not appear in the dvc.yaml, but only in `params:MD+thermostat`
+    # how to handle deps going into the thermostat node?
+    
+    assert thermostat.name == "MD+thermostat"
     assert md.name == "MD"
+    assert md.thermostat.name == "MD+thermostat"
 
 
 if __name__ == "__main__":
