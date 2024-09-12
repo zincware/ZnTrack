39de23b523bc7f16af0e7fe8e141b79717a44ab9
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-09-12 15:58:48 +0200

    test mlflow in CI

---
+++ tests/integration/test_plugins_mlflow.py
@@ -0,0 +1,45 @@
+import pytest
+import os
+import zntrack.examples
+import uuid
+import yaml
+import pathlib
+import mlflow
+
+@pytest.fixture
+def mlflow_proj_path(proj_path):
+    os.environ["ZNTRACK_PLUGINS"] = (
+        "zntrack.plugins.dvc_plugin.DVCPlugin,zntrack.plugins.mlflow_plugin.MLFlowPlugin"
+    )
+    os.environ["MLFLOW_TRACKING_URI"] = "http://127.0.0.1:5000"
+    os.environ["MLFLOW_EXPERIMENT_NAME"] = f"test-{uuid.uuid4()}"
+    
+    config = {
+        "global":
+        {
+            "ZNTRACK_PLUGINS": os.environ["ZNTRACK_PLUGINS"],
+            "MLFLOW_TRACKING_URI": os.environ["MLFLOW_TRACKING_URI"],
+            "MLFLOW_EXPERIMENT_NAME": os.environ["MLFLOW_EXPERIMENT_NAME"],
+        }
+    }
+    pathlib.Path("env.yaml").write_text(yaml.dump(config))
+
+    yield proj_path
+
+    del os.environ["ZNTRACK_PLUGINS"]
+    del os.environ["MLFLOW_TRACKING_URI"]
+    # del os.environ["MLFLOW_EXPERIMENT_NAME"]
+
+
+def test_mlflow_metrics(mlflow_proj_path):
+    proj = zntrack.Project()
+
+    with proj:
+        node = zntrack.examples.ParamsToOuts(params=1)
+    
+    proj.repro()
+
+    # list all mlflow runs under the active experiment
+    runs = mlflow.search_runs()
+    # one parent and one child run
+    assert len(runs) == 2 
