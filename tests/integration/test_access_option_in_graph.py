5213e60b0c63737053d92f92a7fc1dedb53e27e4
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2024-09-12 14:28:14 +0200

    Pass `node.name` to another node in graph

---
+++ tests/integration/test_access_option_in_graph.py
@@ -0,0 +1,20 @@
+import zntrack.examples
+
+def test_connect_node_names(proj_path):
+    with zntrack.Project() as proj:
+        a = zntrack.examples.ParamsToOuts(params=10, name="A")
+        b = zntrack.examples.ParamsToOuts(params=20, name=f"con_{a.name}")
+    proj.build()
+
+    assert a.name == "A"
+    assert b.name == "con_A"
+
+def test_connect_node_names_automatic_names(proj_path):
+    with zntrack.Project() as proj:
+        a = zntrack.examples.ParamsToOuts(params=10)
+        b = zntrack.examples.ParamsToOuts(params=20, name=f"con_{a.name}")
+    proj.build()
+
+    assert a.name == "ParamsToOuts"
+    assert b.name == "con_ParamsToOuts"
+    
