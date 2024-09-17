4bae5d596ce62482141425503f6be2bfa08b79aa
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-09-17 11:13:49 +0200

    test generic deps types

---
+++ tests/unit_tests/test_deps_types.py
@@ -0,0 +1,22 @@
+import zntrack
+import typing as t
+import pytest
+
+class AcceptDeps(zntrack.Node):
+    deps: t.Any = zntrack.deps()
+
+
+def test_generic_deps(proj_path):
+    with zntrack.Project() as proj:
+        AcceptDeps(deps=1)
+    
+    with pytest.raises(ValueError):
+        proj.build()
+
+
+def test_generic_deps_list(proj_path):
+    with zntrack.Project() as proj:
+        AcceptDeps(deps=[1, 2, 3])
+    
+    with pytest.raises(ValueError):
+        proj.build()
