4cfe2c41aadc3ea08f935783402163b0c9012736
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-10-17 08:59:45 +0200

    remove remote package import

--- zntrack/from_rev.py
+++ zntrack/from_rev.py
@@ -3,55 +3,10 @@ import importlib
 import importlib.util
 import pathlib
 import sys
-import tempfile
-import uuid
 
 import dvc.api
 import dvc.repo
 import dvc.stage
-
-
-def _import_from_tempfile(package_and_module: str, remote, rev):
-    """Create a temporary file to import from.
-
-    Parameters
-    ----------
-    package_and_module : str
-        The package and module to import, e.g. "zntrack.core.node.Node".
-    remote : str
-        The remote to load the module from.
-    rev : str
-        The revision to load the module from.
-
-    Returns
-    -------
-    ModuleType
-        The imported module.
-
-    Raises
-    ------
-    ModuleNotFoundError
-        If the module could not be found.
-    FileNotFoundError
-        If the file could not be found.
-
-    """
-    # TODO: either make this a proper tested package
-    # or remove this feature for security reasons
-    file = pathlib.Path(*package_and_module.split(".")).with_suffix(".py")
-    fs = dvc.api.DVCFileSystem(url=remote, rev=rev)
-    with tempfile.NamedTemporaryFile(suffix=".py") as temp_file, fs.open(file) as f:
-        temp_file.write(f.read())
-        temp_file.flush()
-
-        # we use a random uuid to avoid name clashes
-        ref_module = f"{uuid.uuid4()}.{package_and_module}"
-
-        spec = importlib.util.spec_from_file_location(ref_module, temp_file.name)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[ref_module] = module
-        spec.loader.exec_module(module)
-        return module
 
 
 def from_rev(name: str, remote: str | None = None, rev: str | None = None):
@@ -78,14 +33,11 @@ def from_rev(name: str, remote: str | None = None, rev: str | None = None):
     try:
         module = importlib.import_module(package_and_module)
     except ModuleNotFoundError:
-        try:
-            module = _import_from_tempfile(package_and_module, remote, rev)
-        except (FileNotFoundError, ModuleNotFoundError):
-            module_name = package_and_module.split(".")[0]
-            raise ModuleNotFoundError(
-                f"No module named '{module_name}'. The package might be available via 'pip"
-                f" install {module_name}' or from the remote via 'pip install git+{remote}'."
-            )
+        raise ModuleNotFoundError(
+            f"No module found for '{package_and_module}'. The package might be available "
+            f"via 'pip install {package_and_module}' or from the remote" 
+            f"via 'pip install git+{remote}'."
+        )
 
     cls = getattr(module, cls_name)
     return cls.from_rev(name, remote=remote, rev=rev)
