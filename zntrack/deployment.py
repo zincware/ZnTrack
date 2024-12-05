ea97358502e6e4fc253e5d32bd1254576dea1ed1
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-12-05 10:38:41 +0100

    update deployment to also save runtime

--- zntrack/deployment.py
+++ zntrack/deployment.py
@@ -1,4 +1,5 @@
 import unittest.mock as mock
+import datetime
 
 from znflow.deployment import VanillaDeployment
 
@@ -6,7 +7,9 @@ from znflow.deployment import VanillaDeployment
 class ZnTrackDeployment(VanillaDeployment):
     def _run_node(self, node_uuid):
         node = self.graph.nodes[node_uuid]["value"]
+        start_time = datetime.datetime.now()
         node.state.increment_run_count()
+        node.state.save_node_meta()
         if hasattr(node, "_method"):
             method_string = getattr(node, "_method")
             method = getattr(node, method_string)
@@ -20,6 +23,8 @@ class ZnTrackDeployment(VanillaDeployment):
         else:
             super()._run_node(node_uuid)
 
+        run_time = datetime.datetime.now() - start_time
+        node.state.add_run_time(run_time)
         node.state.save_node_meta()
         node.save()
 
