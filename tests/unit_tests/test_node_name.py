77599bc8bff01685e4a86c32485c2e50bdb0761c
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-02-01 10:16:18 +0100

    update unit name test

--- tests/unit_tests/test_node_name.py
+++ tests/unit_tests/test_node_name.py
@@ -14,6 +14,14 @@ def test_node_name(proj_path):
 
     assert n.name == "MyNode"
 
+def test_custom_node_name(proj_path):
+    project = zntrack.Project()
+    with project:
+        n = MyNode(name="CustomName")
+        assert n.name == "CustomName"
+    
+    assert n.name == "CustomName"
+
 
 def test_duplicate_node_name(proj_path):
     with zntrack.Project():
@@ -56,9 +64,62 @@ def test_grouped_node_name(proj_path):
     assert n4.name == "grp1_MyNode_1"
 
 
+def test_nested_grouped_node_name(proj_path):
+    project = zntrack.Project()
+
+    with project.group("A", "B"):
+        n1 = MyNode()
+        assert n1.name == "A_B_MyNode"
+        n2 = MyNode()
+        assert n2.name == "A_B_MyNode_1"
+
+    assert n1.name == "A_B_MyNode"
+    assert n2.name == "A_B_MyNode_1"
+
+def test_grouped_custom_node_name(proj_path):
+    project = zntrack.Project()
+
+    with project.group("grp1"):
+        n1 = MyNode(name="Alpha")
+        n2 = MyNode(name="Beta")
+
+        assert n1.name == "grp1_Alpha"
+        assert n2.name == "grp1_Beta"
+    
+    assert n1.name == "grp1_Alpha"
+    assert n2.name == "grp1_Beta"
+
+def test_nested_grouped_custom_node_name(proj_path):
+    project = zntrack.Project()
+
+    with project.group("A", "B"):
+        n1 = MyNode(name="Alpha")
+        n2 = MyNode(name="Beta")
+
+        assert n1.name == "A_B_Alpha"
+        assert n2.name == "A_B_Beta"
+    
+    assert n1.name == "A_B_Alpha"
+    assert n2.name == "A_B_Beta"
+
 def test_duplicate_named_node():
     with zntrack.Project() as proj:
         n1 = MyNode(name="A")
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="A node with the name 'A' already exists."):
         with proj:
             n2 = MyNode(name="A")
+
+def test_grouped_duplicate_named_node():
+    project = zntrack.Project()
+
+    with project.group("grp1"):
+        n1 = MyNode(name="A")
+        assert n1.name == "grp1_A"
+    
+    with project.group("grp2"):
+        n2 = MyNode(name="A")
+        assert n2.name == "grp2_A"
+
+    with project.group("grp1"):
+        with pytest.raises(ValueError, match="A node with the name 'grp1_A' already exists."):
+            n3 = MyNode(name="A")
