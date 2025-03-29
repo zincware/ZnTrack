7916ae56da76c85b7009d4142bf0345b2bfbdd6b
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-03-29 11:17:40 +0100

    add benchmark

---
+++ tests/benchmark/test_benchmark.py
@@ -0,0 +1,58 @@
+import zntrack.examples
+import pytest
+import random
+import numpy as np
+
+@pytest.mark.benchmark(group="node-count")
+@pytest.mark.parametrize("count", np.arange(0, 1100, 100)[1:])
+def test_node_count(benchmark, count, tmp_path):
+    """
+    Benchmark the number of nodes in a graph.
+    """
+
+    def _build():
+        project = zntrack.Project()
+        with project:
+            for _ in range(count):
+                zntrack.examples.AddNumbers(
+                    a=random.randint(1, 1000),
+                    b=random.randint(1, 1000),
+                )
+        project.build()
+    
+    benchmark(_build)
+
+
+@pytest.mark.benchmark(group="edge-count")
+@pytest.mark.parametrize("count", np.arange(0, 110, 10)[1:])
+def test_connections(benchmark, count, tmp_path):
+    """
+    Benchmark the number of connections in a graph.
+    """
+
+    # TODO: there might be a difference in performance depending on 
+    # the longest_path in the DAG or even the degree per node.
+
+    def _build():
+        project = zntrack.Project()
+        assert count < 1000, "Count must be less than 1000"
+        initial_nodes = 1000 - count
+        nodes = []
+        with project:
+            for _ in range(initial_nodes):
+                nodes.append(zntrack.examples.AddNumbers(
+                    a=random.randint(1, 1000),
+                    b=random.randint(1, 1000),
+                ))
+            for _ in range(count):
+                zntrack.examples.AddNodes(
+                    a=random.sample(nodes, 1),
+                    b=None,
+                )
+        
+        assert len(project.nodes) == 1000
+        assert len(project.edges) == count
+
+        project.build()
+    
+    benchmark(_build)
