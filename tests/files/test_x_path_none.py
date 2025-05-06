6e62110007fe29e2902850fedc81d5b08e9f3485
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-05-06 11:05:58 +0200

    support `None` in `x_path`

--- tests/files/test_x_path_none.py
+++ tests/files/test_x_path_none.py
@@ -1,5 +1,15 @@
 import zntrack
 import typing as t
+import json
+import pathlib
+
+import pandas as pd
+import yaml
+
+import zntrack
+import zntrack.group
+
+CWD = pathlib.Path(__file__).parent.resolve()
 
 class MyNode(zntrack.Node):
     deps_path: t.Union[list[t.Union[str, None]], None] = zntrack.deps_path()
@@ -11,7 +21,7 @@ class MyNode(zntrack.Node):
     def run(self):
         pass
 
-def test_x_path_none():
+def test_x_path_none(proj_path):
     project = zntrack.Project()
 
     with project:
@@ -22,12 +32,22 @@ def test_x_path_none():
             metrics_path=None,
             plots_path=None,
         )
-        # node_list_none = MyNode(
-        #     deps_path=[None],
-        #     params_path=[None],
-        #     outs_path=[None],
-        #     metrics_path=[None],
-        #     plots_path=[None],
-        # )
+        node_list_none = MyNode(
+            deps_path=[None],
+            params_path=[None],
+            outs_path=[None],
+            metrics_path=[None],
+            plots_path=[None],
+        )
 
     project.build()
+
+    assert json.loads((CWD / "zntrack_config" / "x_path_none.json").read_text()) == json.loads(
+        (proj_path / "zntrack.json").read_text()
+    )
+    assert yaml.safe_load(
+        (CWD / "dvc_config" / "x_path_none.yaml").read_text()
+    ) == yaml.safe_load((proj_path / "dvc.yaml").read_text())
+    assert (CWD / "params_config" / "x_path_none.yaml").read_text() == (
+        proj_path / "params.yaml"
+    ).read_text()
