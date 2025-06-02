801279252924a2d807c4961559222cba71b0124d
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-06-02 17:34:11 +0200

    format / ready for merge

--- tests/integration/test_outs_path_deps_path.py
+++ tests/integration/test_outs_path_deps_path.py
@@ -4,15 +4,19 @@ import zntrack.examples
 
 
 def test_outs_path_to_deps_path(proj_path):
+    (proj_path / "output.txt").write_text("15")
+
     with zntrack.Project() as proj:
         a = zntrack.examples.WriteDVCOuts(params=10)
-        # assert a.outs == Path("nodes/WriteDVCOuts/output.txt") # uses znflow.resolve
         b = zntrack.examples.ReadFile(path=a.outs)
-        # b = zntrack.examples.ReadFile(path=znflow.resolve(a.outs))
-        # b = zntrack.examples.ReadFile(path=Path("nodes/WriteDVCOuts/output.txt")) # works
+        #
+        c = zntrack.examples.ReadFile(path=Path("output.txt"))
 
     proj.repro()
 
     assert a.outs == Path("nodes/WriteDVCOuts/output.txt")
     assert b.path == Path("nodes/WriteDVCOuts/output.txt")
     assert b.content == "10"
+    # control
+    assert c.path == Path("output.txt")
+    assert c.content == "15"
